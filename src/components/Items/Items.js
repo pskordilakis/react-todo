2cffa000f41dd45e579e4dc1f471f060220b631e
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2019-01-21 11:04:17 +0200

    setup basic structure

---
+++ src/components/Items/Items.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Items'
+
+class Items extends Component {
+  render () {
+    return (
+      <div className="Items">
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+      </div>
+    )
+  }
+}
+
+export default Items
---
+++ src/components/Search/Search.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class Search extends Component {
+    render () {
+        return (
+            <div className="Search">
+                <input type="search" />
+            </div>
+        )
+    }
+}
+
+export default Search
