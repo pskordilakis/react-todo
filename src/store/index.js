8447af26c741c34468c4cd01a9e37ba3668ad3b5
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2019-01-22 16:46:11 +0200

    add remote redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,21 @@
 import { createStore } from 'redux'
+import devToolsEnhancer from 'remote-redux-devtools'
 import app from './reducers'
 
-const store = createStore(app, {
-  items: [
-    { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-    { text: 'Proin consectetur nec ipsum sed eleifend.' },
-    { text: 'Praesent scelerisque dui sed lectus lobortis porttitor.' },
-    { text: 'Fusce non luctus sapien.' },
-    { text: 'In vitae tempus ante.' },
-    { text: 'Aliquam vel lobortis nisl.' },
-    { text: 'Nulla facilisi.' },
-  ]
-})
+const store = createStore(app,
+  {
+    items: [
+      { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+      { text: 'Proin consectetur nec ipsum sed eleifend.' },
+      { text: 'Praesent scelerisque dui sed lectus lobortis porttitor.' },
+      { text: 'Fusce non luctus sapien.' },
+      { text: 'In vitae tempus ante.' },
+      { text: 'Aliquam vel lobortis nisl.' },
+      { text: 'Nulla facilisi.' },
+    ],
+  },
+  devToolsEnhancer()
+)
 
 export default store
 
